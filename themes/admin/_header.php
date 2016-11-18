dae89fcda5e93d8a6b747434c56ff01f7c3e663a
unisexx <unisexx@gmail.com>
2016-11-18 15:16:40 +0700

    ....

---
+++ themes/admin/_header.php
@@ -0,0 +1,12 @@
+<div id='cssmenu'>
+<ul>
+	<li class="active"><a href='admin/user'><span>ไฮไลท์</span></a></li>
+	<li><a href='admin/user'><span>ข่าวประชาสัมพันธ์</span></a></li>
+	<li><a href='admin/user'><span>เว็บบอร์ด</span></a></li>
+	<li><a href='admin/user'><span>แบนเนอร์</span></a></li>
+	<li><a href='admin/user'><span>ผู้ใช้งาน</span></a></li>
+	<li><a href='users/logout'><span>ออกจากระบบ</span></a></li>
+</ul>
+</div>
+
+<div class="clear" style="margin-bottom:20px;"></div>
