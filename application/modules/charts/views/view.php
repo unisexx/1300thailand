ecf6c5043521388a4c2c7b999f3e2c4233518cd4
unisexx <unisexx@hotmail.com>
2017-01-31 11:37:31 +0700

    ....

--- application/modules/charts/views/view.php
+++ application/modules/charts/views/view.php
@@ -6,7 +6,13 @@
 		<a href="charts/view/<?=$row->id?>"><button type="submit" class="btn-stat01"><?=$row->name?></button></a>
 	<?endforeach;?>
 	<div class="clearfix">&nbsp;</div>
-	
+	<a href="http://moc.m-society.go.th/?page_id=679" target="_blank">
+		<div class="barcode">
+			<img src="themes/1300/images/1300-barcode.jpg" ><br><br>
+			http://moc.m-society.go.th/?page_id=679
+		</div>
+	</a>
+	<br>
 	<table class="table table-bordered table-striped">
 		<thead>
 		<tr>
